a9328bd632cd7ce34e243f39bfaf9c7b83b03250
Jose <joseurio150@gmail.com>
2023-04-17 10:55:45 -0400

    added procfile

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const {Client} = require('pg')
 const client = new Client({
     host: "whereru.cui0zqioehzd.us-east-2.rds.amazonaws.com",
     user: "postgres",
-    port: '5432',
+    port : 80,
     password: "password1",
     database: "postgres"
 })
