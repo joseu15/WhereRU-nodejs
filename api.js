4379c4fed30a93580487111e4a4dd5a6f25d7923
Jose <joseurio150@gmail.com>
2023-04-18 15:15:15 -0400

    added more of the desired functionality

--- api.js
+++ api.js
@@ -71,7 +71,7 @@ app.post('/createAccount/:username/:password',(req,res)=>{
 app.post('/insertPost', (req, res)=> {
     client.query(`INSERT INTO posts SELECT max(id)+1, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 FROM posts`,[req.body.username,req.body.text,req.body.radius,0,req.body.tag1,req.body.tag2,req.body.tag3,req.body.tag4,req.body.tag5,req.body.lat,req.body.lon,req.body.time], (err, result)=>{
         if(!err){
-            res.send('Insertion was successful')
+            res.send(result.rows)
         }
         else{ console.log(err.message) }
     })
