949c0ca68bde00eb3a89feb402b96f43ecb14fe3
Jose <joseurio150@gmail.com>
2023-04-19 00:30:17 -0400

    added more of the desired functionality

--- api.js
+++ api.js
@@ -59,15 +59,13 @@ app.post('/getNearby',(req,res)=>{
 
 //User should check if the username is taken before attempting or will get error
 app.post('/createAccount/:username/:password',(req,res)=>{
-    client.query('INSERT INTO logins SELECT $1,$2 WHERE NOT EXISTS (SELECT 1 FROM logins WHERE username = $3)',[req.params.username,req.params.password,req.params.username],(err,result)=>{
+    client.query('INSERT INTO logins VALUES($1,$2)',[req.params.username,req.params.password],(err,result)=>{
         if (!err){
-            console.log(result.rows + result.insertId);
             res.send('Account created successfully');
-            
         }
         else{
             console.log(err.message + ":(");
-            res.send("1");
+            res.send(err.message);
         } 
     });
 })
